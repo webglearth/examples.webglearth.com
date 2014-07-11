64c380111b2cfc5f393a2b7ecd9dd03f6413191f
dala <dalibor.janak@gmail.com>
2014-07-11 12:35:37 +0200

    Added links to each example. Use #name-of-example-file after link

--- js/index.js
+++ js/index.js
@@ -5,34 +5,38 @@
 
 $(document).ready(function() {
 
-  //load hello world by default
-  loadCode('examples/helloworld.html');
-
-  //event for swithing examples
+//event for swithing examples
   $('.link').each(function() {
     $(this).on('click', function() {
       var name = $(this).text();
       var link = $(this).attr('href');
       var desc = $(this).data('desc');
-
-      $('.view-example').each(function() {
-        $(this).attr('href', link);
-      });
-      $('.active').prop('class', '');
-      $(this).prop('class', 'active');
-
-      //fill page attributes
-      $('#subtitle').text(name);
-      $('#view').prop('src', link);
-      $('#desc').html('<p>' + desc + '</p>');
-
-      //reset copybutton
-      $('#btn-copy').html('Copy');
-      loadCode(link);
+      updateContent(this, name, link, desc);
 
       return false;
     });
   });
+
+  function updateContent(elem, name, link, desc) {
+    $('.view-example').each(function() {
+      $(this).prop('href', link);
+    });
+    //add link to example
+    var url = $(location).prop('href');
+    var urlParts = link.split('/');
+    window.location.hash = urlParts[1].slice(0,-5);
+    //select active
+    $('.active').prop('class', '');
+    $(elem).prop('class', 'active');
+    //fill page attributes
+    $('#subtitle').text(name);
+    $('#view').prop('src', link);
+    $('#desc').html('<p>' + desc + '</p>');
+    //reset copybutton
+    $('#btn-copy').html('Copy');
+    loadCode(link);
+  }
+
 
   //load file with AJAX
   function loadCode(link) {
@@ -41,19 +45,34 @@ $(document).ready(function() {
     }).done(function(results) {
       $('#source').text(results);
       $('#btn-copy').attr('data-clipboard-text', results);
+    }).fail(function() {
+      alert('Can\'t load file ' + link);
     });
+    console.log('ajax');
   }
 
+  //init content
+  var url = $(location).prop('href');
+  var path = url.split('#');
+  if (path[1] !== undefined) {
+    var link = 'examples/' + path[1] + '.html';
+    var elem = $('.link[href="' + link + '"]');
+    console.log(elem);
+    var name = $(elem).text();
+    var desc = $(elem).data('desc');
+    updateContent(elem, name, link, desc);
+    loadCode(link);
+    console.log(link);
+  } else {
+    loadCode('examples/helloworld.html');
+  }
 
-
-  //copy to clipboard
+//copy to clipboard
   ZeroClipboard.config({swfPath: 'http://wmts.maptiler.com/swf/ZeroClipboard.swf'});
-
   var btnCopy = new ZeroClipboard(document.getElementById('btn-copy'));
   btnCopy.on('ready', function(readyEvent) {
     btnCopy.on('aftercopy', function(event) {
       document.getElementById('btn-copy').innerText = 'Copied!';
     });
   });
-
 });
